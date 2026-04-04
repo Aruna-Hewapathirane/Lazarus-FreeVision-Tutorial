380e424d7c03b4af8a93184e236285f9ad24d9a5
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-04-04 04:09:58 -0400

    Update Project1.pas

--- 02_-_Status line_and_Menu/10_-_Menu/Project1.pas
+++ 02_-_Status line_and_Menu/10_-_Menu/Project1.pas
@@ -1,81 +1,91 @@
 //image image.png
 (*
-Hinzufügen eines Menüs.
+Add a menu.
 *)
-//lineal
+//ruleral
 program Project1;
 
 (*
-Für das Menü werden die gleichen Units wie für die Statuszeile gebraucht.
+The same units are needed for the menu as for the status bar.
 *)
 //code+
 uses
-  App,      // TApplication
-  Objects,  // Fensterbereich (TRect)
-  Drivers,  // Hotkey
-  Views,    // Ereigniss (cmQuit)
-  Menus;    // Statuszeile
+App,     // TApplication
+Objects, // Window area (TRect)
+Drivers, // Hotkey
+Views,   // Event (cmQuit)
+Menus;   // Status bar
 //code-
 
 (*
-Für ein Menu muss man <b>InitMenuBar</b> vererben.
+For a menu, you must inherit <b>InitMenuBar</b>.
 *)
 //code+
 type
-  TMyApp = object(TApplication)
-    procedure InitStatusLine; virtual;   // Statuszeile
-    procedure InitMenuBar; virtual;      // Menü
-  end;
-  //code-
+TMyApp = object(TApplication)
+procedure InitStatusLine; virtual; // Status bar
+procedure InitMenuBar; virtual;    // Menu
+end;
 
-  procedure TMyApp.InitStatusLine;
-  var
-    R: TRect;                 // Rechteck für die Statuszeilen Position.
+//code-
 
-    P0: PStatusDef;           // Pointer ganzer Eintrag.
-    P1, P2, P3: PStatusItem;  // Pointer auf die einzelnen Hot-Key.
-  begin
-    GetExtent(R);             // Liefert die Grösse/Position der App, im Normalfall 0, 0, 80, 24.
-    R.A.Y := R.B.Y - 1;       // Position der Statuszeile, auf unterste Zeile der App setzen.
-
-    P3 := NewStatusKey('~F1~ Hilfe', kbF1, cmHelp, nil);
-    P2 := NewStatusKey('~F10~ Menu', kbF10, cmMenu, P3);
-    P1 := NewStatusKey('~Alt+X~ Programm beenden', kbAltX, cmQuit, P2);
-    P0 := NewStatusDef(0, $FFFF, P1, nil);
-
-    StatusLine := New(PStatusLine, Init(R, P0));
-  end;
-
-(*
-Das Menü erzeugen, das Beispiel hat nur eine einziger Menüpunkt, Beenden.
-Beim Menü sind die Zeichen die mit <b>~x~</b> hervorgehoben sind nicht nur Optischen, sonder auch funktionell.
-Zum beenden, kann man auch <b>[Alt+s]</b>, <b>[b]</b> drücken.
-Es gibt auch direkte HotKey auf die Menüpunkte, hier im Beipiel ist die <b>[Alt+x]</b> für beenden.
-Dieses überschneidet sich hier zufällig mit <b>[Alt+x]</b> von der Statuszeile, aber dies ist egal.
-Der Aufbau der Menüerzeugung ist ähnlich der Statuszeile.
-Beim letzten Menüpunkt kommt immer ein <b>nil</b>.
-*)
-
-  //code+
-  procedure TMyApp.InitMenuBar;
-  var
-    R: TRect;           // Rechteck für die Memüzeile Position.
-  begin
-    GetExtent(R);
-    R.B.Y := R.A.Y + 1; // Position des Menüs, auf oberste Zeile der App setzen.
-
-    MenuBar := New(PMenuBar, Init(R, NewMenu(
-      NewSubMenu('~D~atei', hcNoContext, NewMenu(
-      NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcNoContext,
-      nil)), nil))));
-  end;
-  //code-
+procedure TMyApp.InitStatusLine;
 
 var
-  MyApp: TMyApp;
+R: TRect;                // Rectangle for the status bar position.
+P0: PStatusDef;          // Pointer to the entire entry.
+P1, P2, P3: PStatusItem; // Pointer to the individual hotkeys.
 
 begin
-  MyApp.Init;   // Inizialisieren
-  MyApp.Run;    // Abarbeiten
-  MyApp.Done;   // Freigeben
+
+GetExtent(R); // Returns the size/position of the app, normally 0, 0, 80, 24.
+R.A.Y := R.B.Y - 1; // Position of the status bar, set to the bottom line of the app.
+P3 := NewStatusKey('~F1~ Help', kbF1, cmHelp, nil);
+P2 := NewStatusKey('~F10~ Menu', kbF10, cmMenu, P3);
+P1 := NewStatusKey('~Alt+X~ Exit Program', kbAltX, cmQuit, P2);
+P0 := NewStatusDef(0, $FFFF, P1, nil);
+StatusLine := New(PStatusLine, Init(R, P0));
+end;
+
+(*
+Create the menu; this example has only one menu item: Exit.
+In the menu, the characters highlighted with <b>~x~</b> are not only visual but also functional.
+To exit, you can also press <b>[Alt+s]</b> or <b>[b]</b>.
+There are also direct hotkeys for the menu items; in this example, <b>[Alt+x]</b> is for exiting.
+This happens to overlap with <b>[Alt+x]</b> from the status bar, but this doesn't matter.
+The menu creation structure is similar to the status bar.
+The last menu item always contains <b>nil</b>.
+
+*)
+
+//code+
+
+procedure TMyApp.InitMenuBar;
+
+var
+
+R: TRect; // Rectangle for the menu bar position.
+
+begin
+
+GetExtent(R);
+
+R.B.Y := R.A.Y + 1; // Set the menu position to the top row of the app.
+
+MenuBar := New(PMenuBar, Init(R, NewMenu(
+NewSubMenu('~D~atei', hcNoContext, NewMenu(
+NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcNoContext,
+nil)), nil))));
+
+end;
+
+/code-
+
+var
+MyApp: TMyApp;
+
+begin
+MyApp.Init; // Initialize
+MyApp.Run; // Execute
+MyApp.Done; // Release
 end.
