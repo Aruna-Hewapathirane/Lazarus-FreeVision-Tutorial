b2a0c08c242665fb78bd0f44660800086dc6f5ca
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-04-04 04:04:23 -0400

    Update Project1.pas

--- 02_-_Status line_and_Menu/05_-_Status_Line_multiple_entries/Project1.pas
+++ 02_-_Status line_and_Menu/05_-_Status_Line_multiple_entries/Project1.pas
@@ -1,57 +1,64 @@
 //image image.png
 (*
-Ändern der Status-Zeile, mit mehreren Optionen.
+Change the status bar, with multiple options.
 *)
-//lineal
+//ruleral
 program Project1;
 
 uses
-  App,      // TApplication
-  Objects,  // Fensterbereich (TRect)
-  Drivers,  // Hotkey
-  Views,    // Ereigniss (cmQuit)
-  Menus;    // Statuszeile
+
+App,     // TApplication
+Objects, // Window area (TRect)
+Drivers, // Hotkey
+Views,   // Event (cmQuit)
+Menu;    // Status bar
 
 type
-  TMyApp = object(TApplication)
-    procedure InitStatusLine; virtual;
-  end;
+TMyApp = object(TApplication)
+procedure InitStatusLine; virtual;
+end;
 
 (*
-In der Statuszeile sind auch mehrere Hot-Key möglich.
-Die Deklaration könnte man verschachtelt in einer Zeile schreiben.
-Im Beispiel wird es gesplittet gemacht.
+Multiple hotkeys are possible in the status bar.
+The declaration could be nested on a single line.
+In this example, it is split.
 *)
-  //code+
-  procedure TMyApp.InitStatusLine;
-  var
-    R: TRect;                 // Rechteck für die Statuszeilen Position.
 
-    P0: PStatusDef;           // Pointer ganzer Eintrag.
-    P1, P2, P3: PStatusItem;  // Pointer auf die einzelnen Hot-Key.
-  begin
-    GetExtent(R);             // Liefert die Grösse/Position der App, im Normalfall 0, 0, 80, 24.
-    R.A.Y := R.B.Y - 1;       // Position der Statuszeile, auf unterste Zeile der App setzen.
+/code+
+procedure TMyApp.InitStatusLine;
 
-    P3 := NewStatusKey('~F1~ Hilfe', kbF1, cmHelp, nil);
-    P2 := NewStatusKey('~F10~ Menu', kbF10, cmMenu, P3);
-    P1 := NewStatusKey('~Alt+X~ Programm beenden', kbAltX, cmQuit, P2);
-    P0 := NewStatusDef(0, $FFFF, P1, nil);
-
-    StatusLine := New(PStatusLine, Init(R, P0));
-  end;
-  //code-
-
-(*
-Die Deklaration und Ausführung bleibt gleich.
-*)
-  //code+
 var
-  MyApp: TMyApp;
+
+R: TRect; // Rectangle for the status bar position.
+P0: PStatusDef; // Pointer to the entire entry.
+P1, P2, P3: PStatusItem; // Pointer to the individual hotkeys.
 
 begin
-  MyApp.Init;   // Inizialisieren
-  MyApp.Run;    // Abarbeiten
-  MyApp.Done;   // Freigeben
+GetExtent(R); // Returns the size/position of the app, typically 0, 0, 80, 24.
+R.A.Y := R.B.Y - 1; // Position of the status bar, set to the bottom line of the app.
+P3 := NewStatusKey('~F1~ Help', kbF1, cmHelp, nil);
+P2 := NewStatusKey('~F10~ Menu', kbF10, cmMenu, P3);
+P1 := NewStatusKey('~Alt+X~ Exit Program', kbAltX, cmQuit, P2);
+P0 := NewStatusDef(0, $FFFF, P1, nil);
+StatusLine := New(PStatusLine, Init(R, P0));
+end;
+
+//code-
+
+(*
+The declaration and execution remain the same.
+*)
+
+//code+
+
+var
+
+MyApp: TMyApp;
+
+begin
+MyApp.Init; // Initialize
+MyApp.Run; // Execute
+MyApp.Done; // Release
 end.
+
 //code-
