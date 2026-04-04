c22d4a9400c136891d94ce5a9cdde30dbb99dab6
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-04-04 04:22:24 -0400

    Update Project1.pas

--- 02_-_Status line_and_Menu/15_-_Menu_extended/Project1.pas
+++ 02_-_Status line_and_Menu/15_-_Menu_extended/Project1.pas
@@ -1,95 +1,202 @@
 //image image.png
 (*
-Hinzufügen mehrere Menüpunkte.
-Hier wird dies auch der Übersicht zu liebe gesplittet gemacht.
+Add multiple menu items.
+
+Here, this is also done in a split format for clarity.
+
 *)
-//lineal
+//ruleral
 program Project1;
 
 uses
-  App,      // TApplication
-  Objects,  // Fensterbereich (TRect)
-  Drivers,  // Hotkey
-  Views,    // Ereigniss (cmQuit)
-  Menus;    // Statuszeile
+
+App, // TApplication
+
+Objects, // Window area (TRect)
+
+Drivers, // Hotkey
+
+Views, // Event (cmQuit)
+
+Menu; // Status bar
 
 (*
-Für eigene Kommandos, muss man noch Kommdocode definieren.
-Es empfiehlt sich Werte > 1000 zu verwenden, so das es keine Überschneidungen mit den Standard-Codes gibt.
+For custom commands, you need to define command code.
+
+It is recommended to use values ​​> 1000 to avoid conflicts with the standard codes.
+
 *)
 //code+
 const
-  cmList = 1002;      // Datei Liste
-  cmAbout = 1001;     // About anzeigen
-  //code-
+cmList = 1002; // File list
+cmAbout = 1001; // Display About
+
+//code-
 
 (*
-Für ein Menu muss man <b>InitMenuBar</b> vererben.
+For a menu, you need to inherit <b>InitMenuBar</b>.
+
 *)
-  //code+
+
+//code+
 type
-  TMyApp = object(TApplication)
-    procedure InitStatusLine; virtual;   // Statuszeile
-    procedure InitMenuBar; virtual;      // Menü
-  end;
-  //code-
+TMyApp = object(TApplication)
 
-  procedure TMyApp.InitStatusLine;
-  var
-    R: TRect;                 // Rechteck für die Statuszeilen Position.
+procedure InitStatusLine; virtual; // Status bar
 
-    P0: PStatusDef;           // Pointer ganzer Eintrag.
-    P1, P2, P3: PStatusItem;  // Pointer auf die einzelnen Hot-Key.
-  begin
-    GetExtent(R);
-    R.A.Y := R.B.Y - 1;
+procedure InitMenuBar; virtual; // Menu
 
-    P3 := NewStatusKey('~F1~ Hilfe', kbF1, cmHelp, nil);
-    P2 := NewStatusKey('~F10~ Menu', kbF10, cmMenu, P3);
-    P1 := NewStatusKey('~Alt+X~ Programm beenden', kbAltX, cmQuit, P2);
-    P0 := NewStatusDef(0, $FFFF, P1, nil);
+end;
 
-    StatusLine := New(PStatusLine, Init(R, P0));
-  end;
+/code-
 
-(*
-Mam kann die Menüeinträge auch gesplittet über Pointer machen.
-Ob man es verschachtelt oder splittet, ist Geschmacksache.
-Mit <b>NewLine</b> kann man eine Leerzeile einfügen.
-Es empfiehlt sich wen bei einem Menüpunkt ein Dialog aufgeht, Hinter der Bezeichnung <b>...</b> zu schreiben.
-*)
-  //code+
-  procedure TMyApp.InitMenuBar;
-  var
-    R: TRect;                          // Rechteck für die Menüzeilen-Position.
-
-    M: PMenu;                          // Ganzes Menü
-    SM0, SM1,                          // Submenu
-    M0_0, M0_1, M0_2, M1_0: PMenuItem; // Einfache Menüpunkte
-
-  begin
-    GetExtent(R);
-    R.B.Y := R.A.Y + 1;
-
-    M1_0 := NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil);
-    SM1 := NewSubMenu('~H~ilfe', hcNoContext, NewMenu(M1_0), nil);
-
-    M0_2 := NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil);
-    M0_1 := NewLine(M0_2);
-    M0_0 := NewItem('~L~iste', 'F2', kbF2, cmList, hcNoContext, M0_1);
-    SM0 := NewSubMenu('~D~atei', hcNoContext, NewMenu(M0_0), SM1);
-
-    M := NewMenu(SM0);
-
-    MenuBar := New(PMenuBar, Init(R, M));
-  end;
-  //code-
+procedure TMyApp.InitStatusLine;
 
 var
-  MyApp: TMyApp;
+
+R: TRect; // Rectangle for the status bar position.
+
+P0: PStatusDef; // Pointer to the entire entry.
+
+P1, P2, P3: PStatusItem; // Pointer to the individual hotkeys.
 
 begin
-  MyApp.Init;   // Inizialisieren
-  MyApp.Run;    // Abarbeiten
-  MyApp.Done;   // Freigeben
+
+GetExtent(R);
+
+R.A.Y := R.B.Y - 1;
+
+P3 := NewStatusKey('~F1~ Help', kbF1, cmHelp, nil);
+
+P2 := NewStatusKey('~F10~ Menu', kbF10, cmMenu, P3);
+
+P1 := NewStatusKey('~Alt+X~ Exit Program', kbAltX, cmQuit, P2);
+
+P0 := NewStatusDef(0, $FFFF, P1, nil);
+
+StatusLine := New(PStatusLine, Init(R, P0));
+
+end;
+
+(*) Menu items can also be split using pointers.
+Whether you nest or split them is a matter of preference.
+``` can be used to insert a blank line.
+``` is recommended if a dialog box opens for a menu item.
+
+``` after the label is ```.
+
+*)
+
+`/``
+` ...
+``
+``
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+`
+` R.B.Y := R.A.Y + 1; 
+
+M1_0 := NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil); 
+SM1 := NewSubMenu('~Help', hcNoContext, NewMenu(M1_0), nil); 
+
+M0_2 := NewItem('~B~end', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil); 
+M0_1 := NewLine(M0_2); 
+M0_0 := NewItem('~L~list', 'F2', kbF2, cmList, hcNoContext, M0_1); 
+SM0 := NewSubMenu('~File', hcNoContext, NewMenu(M0_0), SM1); 
+
+M := NewMenu(SM0); 
+
+MenuBar := New(PMenuBar, Init(R, M)); 
+end; 
+//code-
+
+var 
+MyApp: TMyApp;
+
+begin 
+MyApp.Init; // Initialize 
+MyApp.Run; // Process 
+MyApp.Done; // Release
 end.
