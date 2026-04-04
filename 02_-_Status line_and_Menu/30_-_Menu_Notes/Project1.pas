d52aefd42906e27122f92d1246083acb7adfc10b
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-04-04 05:45:31 -0400

    Update Project1.pas

--- 02_-_Status line_and_Menu/30_-_Menu_Notes/Project1.pas
+++ 02_-_Status line_and_Menu/30_-_Menu_Notes/Project1.pas
@@ -1,107 +1,149 @@
 //image image.png
 (*
-Hinweise in der Statuszeile der Menü-Punkte.
+Hints in the status bar of the menu items.
+
 *)
-//lineal
+//ruleral
 program Project1;
 
 uses
-  App,      // TApplication
-  Objects,  // Fensterbereich (TRect)
-  Drivers,  // Hotkey
-  Views,    // Ereigniss (cmQuit)
-  Menus;    // Statuszeile
 
-(*
-Konstanten der einzelnen Hilfen.
-Am besten mimmt man da hcxxx Namen.
+App,     // TApplication
+Objects, // Window area (TRect)
+Drivers, // Hotkey
+Views,   // Event (cmQuit)
+Menu;    // Status bar
+
+(*) Constants of the individual help items.
+It's best to use hcxxx names for these.
 *)
 //code+
 const
-  cmList   = 1002;  // Datei Liste
-  cmAbout  = 1001;  // About anzeigen
+cmList = 1002; // File list
+cmAbout = 1001; // Display About
 
-  hcFile   = 10;
-  hcClose  = 11;
-  hcOption = 12;
-  hcFormat = 13;
-  hcEdit   = 14;
-  hcHelp   = 15;
-  hcAbout  = 16;
+hcFile = 10;
+hcClose = 11;
+hcOption = 12;
+hcFormat = 13;
+hcEdit = 14;
+hcHelp = 15;
+hcAbout = 16;
+
 //code-
 
 type
-  TMyApp = object(TApplication)
-    procedure InitStatusLine; virtual;   // Statuszeile
-    procedure InitMenuBar; virtual;      // Menü
-  end;
+
+TMyApp = object(TApplication)
+
+procedure InitStatusLine; virtual; // Status bar
+
+procedure InitMenuBar; virtual; // Menu
+
+end;
 
 (*
-Die Hint-Zeile muss vererbt werden.
-*)
-//code+
-  PHintStatusLine = ^THintStatusLine;
-  THintStatusLine = object(TStatusLine)
-    function Hint(AHelpCtx : Word): ShortString; virtual;
-  end;
+The hint line must be inherited.
 
-  function THintStatusLine.Hint(AHelpCtx: Word): ShortString;
-  begin
-    case AHelpCtx of
-      hcFile:   Hint := 'Dateien verwalten';
-      hcClose:  Hint := 'Programm beenden';
-      hcOption: Hint := 'Verschiedene Optionen';
-      hcFormat: Hint := 'Format einstellen';
-      hcEdit:   Hint := 'Editor Optionen';
-      hcHelp:   Hint := 'Hilfe';
-      hcAbout:  Hint := 'Entwickler Info';
-    else
-      Hint := '';
-    end;
-  end;
+*)
+
+//code+
+
+PHintStatusLine = ^THintStatusLine;
+
+THintStatusLine = object(TStatusLine)
+
+function Hint(AHelpCtx : Word): ShortString; virtual;
+
+end;
+
+function THintStatusLine.Hint(AHelpCtx: Word): ShortString;
+
+begin
+
+case AHelpCtx of
+
+hcFile: Hint := 'Manage files';
+hcClose: Hint := 'Exit program';
+hcOption: Hint := 'Various options';
+hcFormat: Hint := 'Set format';
+hcEdit: Hint := 'Editor options';
+hcHelp: Hint := 'Help';
+hcAbout: Hint := 'Developer Info';
+else
+
+Hint := '';
+
+end;
+
+end;
+
 // code-
 
-  procedure TMyApp.InitStatusLine;
-  var
-    R: TRect;
-  begin
-    GetExtent(R);
-    R.A.Y := R.B.Y - 1;
+procedure TMyApp.InitStatusLine;
 
-    StatusLine := New(PHintStatusLine, Init(R, NewStatusDef(0, $FFFF,
-      NewStatusKey('~Alt+X~ Programm beenden', kbAltX, cmQuit,
-      NewStatusKey('~F10~ Menu', kbF10, cmMenu,
-      NewStatusKey('~F1~ Hilfe', kbF1, cmHelp, nil))), nil)));
-  end;
-(*
-Im Menü muss die hcxxx Konstante mitgegeben werden.
+var
+
+R: TRect;
+
+begin
+
+GetExtent(R);
+
+R.A.Y := R.B.Y - 1;
+
+StatusLine := New(PHintStatusLine, Init(R, NewStatusDef(0, $FFFF,
+
+NewStatusKey('~Alt+X~ Exit Program', kbAltX, cmQuit,
+
+NewStatusKey('~F10~ Menu', kbF10, cmMenu,
+
+NewStatusKey('~F1~ Help', kbF1, cmHelp, nil))), nil)));
+
+end;
+
+(*) The hcxxx constant must be included in the menu.
+
 *)
+
 //code+
-  procedure TMyApp.InitMenuBar;
-  var
-    R: TRect;                   // Rechteck für die Menüzeilen-Position.
-  begin
-    GetExtent(R);
-    R.B.Y := R.A.Y + 1;
 
-    MenuBar := New(PMenuBar, Init(R, NewMenu(
-      NewSubMenu('~D~atei', hcFile, NewMenu(
-        NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcClose, nil)),
+procedure TMyApp.InitMenuBar;
 
-      NewSubMenu('~O~ptionen', hcOption, NewMenu(
-        NewItem('~F~ormat', '', kbNoKey, cmAbout, hcFormat,
-        NewItem('~E~itor', '', kbNoKey, cmAbout, hcEdit, nil))),
+var
 
-      NewSubMenu('~H~ilfe', hcHelp, NewMenu(
-        NewItem('~A~bout...', '', kbNoKey, cmAbout, hcAbout, nil)), nil))))));
-  end;
+R: TRect; // Rectangle for the menu bar position.
+
+begin
+
+GetExtent(R);
+
+R.B.Y := R.A.Y + 1;
+
+
+``` MenuBar := New(PMenuBar, Init(R, NewMenu( 
+NewSubMenu('~File', hcFile, NewMenu( 
+NewItem('~B~end', 'Alt-X', kbAltX, cmQuit, hcClose, nil)), 
+
+NewSubMenu('~Options', hcOption, NewMenu( 
+NewItem('~F~ormat', '', kbNoKey, cmAbout, hcFormat, 
+NewItem('~E~itor', '', kbNoKey, cmAbout, hcEdit, nil))), 
+
+NewSubMenu('~Help', hcHelp, NewMenu( 
+NewItem('~A~bout...', '', kbNoKey, cmAbout, hcAbout, nil)), nil)))))); 
+end;
 //code-
 
 var
-  MyApp: TMyApp;
+
+MyApp: TMyApp;
 
 begin
-  MyApp.Init;   // Inizialisieren
-  MyApp.Run;    // Abarbeiten
-  MyApp.Done;   // Freigeben
+
+MyApp.Init; // Initialize
+
+MyApp.Run; // Execute
+
+MyApp.Done; // Release
+
 end.
