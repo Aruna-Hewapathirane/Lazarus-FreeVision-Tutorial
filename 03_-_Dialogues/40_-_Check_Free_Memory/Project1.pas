384ed4f42c908b6ded15a9d0b01382c937a8fa52
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-04-04 11:46:04 -0400

    Update Project1.pas

--- 03_-_Dialogues/40_-_Check_Free_Memory/Project1.pas
+++ 03_-_Dialogues/40_-_Check_Free_Memory/Project1.pas
@@ -1,218 +1,175 @@
 //image image.png
 (*
-Überprüfen ob genügend Speicher frei ist, um den Dialog zu erzeugen.
-Auf den heutigen Rechner wird die wohl nicht mehr der Fall sein, das der Speicher wegen eines Dialoges überläuft.
+Check whether there is enough memory free to create the dialog.
+On today's computers it will probably no longer be the case that the memory overflows because of a dialog.
 *)
-//lineal
-program Project1;
+//ruler
+programProject1;
 
-uses
-  App,      // TApplication
-  Objects,  // Fensterbereich (TRect)
-  Drivers,  // Hotkey
-  Views,    // Ereigniss (cmQuit)
-  Menus,    // Statuszeile
-  MsgBox,   // Messageboxen
-  Dialogs;  // Dialoge
+uses 
+App, // TApplication 
+Objects, // Window area (TRect) 
+Drivers, // Hotkey 
+Views, // event (cmQuit) 
+Menus, // status line 
+MsgBox, // message boxes 
+dialogue; // Dialogues
 
-const
-  cmAbout = 1001;     // About anzeigen
-  cmList = 1002;      // Datei Liste
-  cmPara = 1003;      // Parameter
+const 
+cmAbout = 1001; // Show About 
+cmList = 1002; // file list 
+cmPara = 1003; // Parameters
 
-type
-  TParameterData = record
-    Druck,
-    Schrift: longword;
-    Hinweis: string[50];
-  end;
+type.type 
+TParameterData = record 
+pressure, 
+font: longword; 
+Note: string[50]; 
+end;
 
 
 (*
-Die virtuelle Procedure <b>OutOfMemory</b>, wen doch mal der Speicher überläuft.
-Wen man diese Methode nicht überschreibt, dann wird keine Fehlermeldung ausgegeben, nur weis dann der Nutzer nicht, wieso sein View nicht erscheint.
-*)
-  //code+
-type
-  TMyApp = object(TApplication)
-    ParameterData: TParameterData;                     // Parameter für Dialog.
-    constructor Init;                                  // Neuer Constructor
+The virtual procedure <b>OutOfMemory</b>, when the memory overflows.
+If you don't overwrite this method, no error message will be output, but the user won't know why their view doesn't appear.
+*) 
+//code+
+type.type 
+TMyApp = object(TApplication) 
+ParameterData: TParameterData; // Parameters for dialog. 
+constructor Init; // New constructor 
 
-    procedure InitStatusLine; virtual;                 // Statuszeile
-    procedure InitMenuBar; virtual;                    // Menü
-    procedure HandleEvent(var Event: TEvent); virtual; // Eventhandler
-    procedure OutOfMemory; virtual;                    // Wird aufgerufen, wen Speicher überläuft.
+procedure InitStatusLine; virtual; // Status line 
+procedure InitMenuBar; virtual; // Menu 
+procedure HandleEvent(var Event: TEvent); virtual; // event handler 
+procedure OutOfMemory; virtual; // Called when memory overflows. 
 
-    procedure MyParameter;                             // neue Funktion für einen Dialog.
-  end;
-  //code-
+procedure MyParameter; // new function for a dialog. 
+end; 
+//code- 
 
-  constructor TMyApp.Init;
-  begin
-    inherited Init;     // Vorfahre aufrufen
-    with ParameterData do begin
-      Druck := %0101;
-      Schrift := 2;
-      Hinweis := 'Hello world';
-    end;
-  end;
+constructor TMyApp.Init; 
+begin 
+inherited Init; // Call ancestor 
+with ParameterData do begin 
+pressure := %0101; 
+font := 2; 
+Note := 'Hello world'; 
+end; 
+end; 
 
-  procedure TMyApp.InitStatusLine;
-  var
-    R: TRect;                 // Rechteck für die Statuszeilen Position.
+procedure TMyApp.InitStatusLine; 
+var 
+R: TRect; // Rectangle for the status line position. 
 
-    P0: PStatusDef;           // Pointer ganzer Eintrag.
-    P1, P2, P3: PStatusItem;  // Poniter auf die einzelnen Hot-Key.
-  begin
-    GetExtent(R);
-    R.A.Y := R.B.Y - 1;
+P0: PStatusDef; // Pointer entire entry. 
+P1, P2, P3: PStatusItem; // Poniter on the individual hot key. 
+begin 
+GetExtent(R); 
+R.A.Y := R.B.Y - 1; 
 
-    P3 := NewStatusKey('~F1~ Hilfe', kbF1, cmHelp, nil);
-    P2 := NewStatusKey('~F10~ Menu', kbF10, cmMenu, P3);
-    P1 := NewStatusKey('~Alt+X~ Programm beenden', kbAltX, cmQuit, P2);
-    P0 := NewStatusDef(0, $FFFF, P1, nil);
+P3 := NewStatusKey('~F1~ Help', kbF1, cmHelp, nil); 
+P2 := NewStatusKey('~F10~ Menu', kbF10, cmMenu, P3); 
+P1 := NewStatusKey('~Alt+X~ Quit program', kbAltX, cmQuit, P2); 
+P0 := NewStatusDef(0, $FFFF, P1, nil); 
 
-    StatusLine := New(PStatusLine, Init(R, P0));
-  end;
+StatusLine := New(PStatusLine, Init(R, P0)); 
+end; 
 
-  procedure TMyApp.InitMenuBar;
-  var
-    R: TRect;                       // Rechteck für die Menüzeilen-Position.
+procedure TMyApp.InitMenuBar; 
+var 
+R: TRect; // Rectangle for the menu line position. 
 
-    M: PMenu;                       // Ganzes Menü
-    SM0, SM1, SM2,                  // Submenu
-    M0_0, M0_2, M0_3, M0_4, M0_5,
-    M1_0, M2_0: PMenuItem;          // Einfache Menüpunkte
+M: PMenu; // Entire menu 
+SM0, SM1, SM2, // Submenu 
+M0_0, M0_2, M0_3, M0_4, M0_5, 
+M1_0, M2_0: PMenuItem; // Simple menu items 
 
-  begin
-    GetExtent(R);
-    R.B.Y := R.A.Y + 1;
+begin 
+GetExtent(R); 
+R.B.Y := R.A.Y + 1; 
 
-    M2_0 := NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil);
-    SM2 := NewSubMenu('~H~ilfe', hcNoContext, NewMenu(M2_0), nil);
+M2_0 := NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil); 
+SM2 := NewSubMenu('~Help', hcNoContext, NewMenu(M2_0), nil); 
 
-    M1_0 := NewItem('~P~arameter...', '', kbF2, cmPara, hcNoContext, nil);
-    SM1 := NewSubMenu('~O~ption', hcNoContext, NewMenu(M1_0), SM2);
+M1_0 := NewItem('~Parameter...', '', kbF2, cmPara, hcNoContext, nil); 
+SM1 := NewSubMenu('~O~ption', hcNoContext, NewMenu(M1_0), SM2); 
 
-    M0_5 := NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil);
-    M0_4 := NewLine(M0_5);
-    M0_3 := NewItem('S~c~hliessen', 'Alt-F3', kbAltF3, cmClose, hcNoContext, M0_4);
-    M0_2 := NewLine(M0_3);
-    M0_0 := NewItem('~L~iste', 'F2', kbF2, cmList, hcNoContext, M0_2);
-    SM0 := NewSubMenu('~D~atei', hcNoContext, NewMenu(M0_0), SM1);
+M0_5 := NewItem('~B~end', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil); 
+M0_4 := NewLine(M0_5); 
+M0_3 := NewItem('S~c~hliessen', 'Alt-F3', kbAltF3, cmClose, hcNoContext, M0_4); 
+M0_2 := NewLine(M0_3); 
+M0_0 := NewItem('~L~iste', 'F2', kbF2, cmList, hcNoContext, M0_2); 
+SM0 := NewSubMenu('~File', hcNoContext, NewMenu(M0_0), SM1); 
 
-    M := NewMenu(SM0);
+M := NewMenu(SM0); 
 
-    MenuBar := New(PMenuBar, Init(R, M));
-  end;
+MenuBar := New(PMenuBar, Init(R, M)); 
+end; 
 
-  procedure TMyApp.HandleEvent(var Event: TEvent);
-  begin
-    inherited HandleEvent(Event);
+procedure TMyApp.HandleEvent(var Event: TEvent); 
+begin 
+inherited HandleEvent(Event); 
 
-    if Event.What = evCommand then begin
-      case Event.Command of
-        cmAbout: begin
-        end;
-        cmList: begin
-        end;
-        cmPara: begin
-          MyParameter;
-        end;
-        else begin
-          Exit;
-        end;
-      end;
-    end;
-    ClearEvent(Event);
-  end;
-
-(*
-Die Procedure wird aufgerufen, wen zu wenig Speicher vorhanden ist.
-*)
-  //code+
-  procedure TMyApp.OutOfMemory;
-  begin
-    MessageBox('Zu wenig Arbeitsspeicher !', nil, mfError + mfOkButton);
-  end;
-  //code-
+if Event.What = evCommand then begin 
+case Event.Command of 
+cmAbout: begin 
+end; 
+cmList: begin 
+end; 
+cmPara: begin 
+MyParameters; 
+end; 
+else begin 
+exit; 
+end; 
+end; 
+end; 
+ClearEvent(Event); 
+end;
 
 (*
-Der Dialog wird jetzt mit Werten geladen.
-Dies macht man, sobald man fertig ist mit Komponenten ertstellen.
-Mit <b>ValidView(...</b> prüft man ob genügend Specher vorhanden ist, um die Komponente zu erzeugen.
-Wen nicht, kommt <b>nil<(b> zurück. Dabei spielt es keine Rolle, ob man <b>OutOfMemory</b> überschreibt.
-*)
-  //code+
-  procedure TMyApp.MyParameter;
-  var
-    Dlg: PDialog;
-    R: TRect;
-    dummy: word;
-    View: PView;
-  begin
-    R.Assign(0, 0, 35, 15);
-    R.Move(23, 3);
-    Dlg := New(PDialog, Init(R, 'Parameter'));
-    with Dlg^ do begin
+The procedure is called when there is not enough memory available.
+*) 
+//code+ 
+procedure TMyApp.OutOfMemory; 
+begin 
+MessageBox('Too little memory !', nil, mfError + mfOkButton); 
+end; 
+//code-
 
-      // CheckBoxen
-      R.Assign(2, 3, 18, 7);
-      View := New(PCheckBoxes, Init(R,
-        NewSItem('~D~atei',
-        NewSItem('~Z~eile',
-        NewSItem('D~a~tum',
-        NewSItem('~Z~eit',
-        nil))))));
-      Insert(View);
-      // Label für CheckGroup.
-      R.Assign(2, 2, 10, 3);
-      Insert(New(PLabel, Init(R, 'Dr~u~cken', View)));
+(*
+The dialog is now loaded with values.
+You do this as soon as you have finished creating components.
+With <b>ValidView(...</b> you check whether there is enough memory available to create the component.
+If not, <b>nil<(b> comes back. It doesn't matter whether you overwrite <b>OutOfMemory</b>.
+*) 
+//code+ 
+procedure TMyApp.MyParameter; 
+var 
+Dlg: PDialog; 
+R: TRect; 
+dummy: word; 
+View: PView; 
+begin 
+R.Assign(0, 0, 35, 15); 
+R.Move(23, 3); 
+Dlg := New(PDialog, Init(R, 'Parameter')); 
+with Dlg^ do begin 
 
-      // RadioButton
-      R.Assign(21, 3, 33, 6);
-      View := New(PRadioButtons, Init(R,
-        NewSItem('~G~ross',
-        NewSItem('~M~ittel',
-        NewSItem('~K~lein',
-        nil)))));
-      Insert(View);
-      // Label für RadioGroup.
-      R.Assign(20, 2, 31, 3);
-      Insert(New(PLabel, Init(R, '~S~chrift', View)));
+// CheckBoxes 
+R.Assign(2, 3, 18, 7); 
+View := New(PCheckBoxes, Init(R, 
+NewSItem('~File', 
+NewSItem('~row~row', 
+NewSItem('D~a~tum', 
+NewSItem('~Time~', 
+nil)))))); 
+Insert(View); 
+// Label for CheckGroup. 
+R.Assign(2, 2, 10, 3); 
+Insert(New(PLabel, Init(R, 'Press', View))); 
 
-      // Edit Zeile
-      R.Assign(3, 10, 32, 11);
-      View := New(PInputLine, Init(R, 50));
-      Insert(View);
-      // Label für Edit Zeile
-      R.Assign(2, 9, 10, 10);
-      Insert(New(PLabel, Init(R, '~H~inweis', View)));
-
-      // Ok-Button
-      R.Assign(7, 12, 17, 14);
-      Insert(new(PButton, Init(R, '~O~K', cmOK, bfDefault)));
-
-      // Schliessen-Button
-      R.Assign(19, 12, 32, 14);
-      Insert(new(PButton, Init(R, '~A~bbruch', cmCancel, bfNormal)));
-    end;
-    if ValidView(Dlg) <> nil then begin // Prüfen ob genügend Speicher.
-      Dlg^.SetData(ParameterData);      // Dialog mit den Werten laden.
-      dummy := Desktop^.ExecView(Dlg);  // Dialog ausführen.
-      if dummy = cmOK then begin        // Wen Dialog mit Ok beenden, dann Daten vom Dialog in Record laden.
-        Dlg^.GetData(ParameterData);
-      end;
-
-      Dispose(Dlg, Done);               // Dialog und Speicher frei geben.
-    end;
-  end;
-  //code-
-var
-  MyApp: TMyApp;
-
-begin
-  MyApp.Init;   // Inizialisieren
-  MyApp.Run;    // Abarbeiten
-  MyApp.Done;   // Freigeben
-end.
+// RadioButton 
+R.Assign(21, 3, 33, 6); 
+View := New(PRadioButtons, Init(R, 
+NewSItem('~G~r
