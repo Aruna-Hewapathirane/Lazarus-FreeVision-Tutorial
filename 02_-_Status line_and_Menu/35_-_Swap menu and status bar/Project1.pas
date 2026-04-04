beac1c868da4c8fef1cfdc4fa739ad2cc275cf92
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-04-04 05:52:39 -0400

    Update Project1.pas

--- 02_-_Status line_and_Menu/35_-_Swap menu and status bar/Project1.pas
+++ 02_-_Status line_and_Menu/35_-_Swap menu and status bar/Project1.pas
@@ -1,172 +1,244 @@
 //image image.png
 (*
-Man kann zur Laufzeit das komplette Menü und Statuszeile austauschen.
-ZB. um die Anwendung mehrsprachig zu machen.
-Dazu wird die aktuelle Komponente entfernt und die neue eingefügt.
-In dem Beispiel gibt es je eine deutsche und englische Komponente.
+The entire menu and status bar can be replaced at runtime.
+
+For example, to make the application multilingual.
+
+To do this, the current component is removed and the new one inserted.
+
+In this example, there is one German and one English component.
+
 *)
-//lineal
+
+//ruleral
 program Project1;
 
 uses
-  App,      // TApplication
-  Objects,  // Fensterbereich (TRect)
-  Drivers,  // Hotkey
-  Views,    // Ereigniss (cmQuit)
-  Menus,    // Statuszeile
-  Dialogs;  // Dialoge
+App, // TApplication
+Objects, // Window area (TRect)
+Drivers, // Hotkey
+Views, // Event (cmQuit)
+Menus, // Status bar
+Dialogs; // Dialogs
 
 const
-  cmAbout = 1001;        // About anzeigen
-  cmPara = 1003;         // Parameter
-  cmMenuEnlish = 1005;   // Englisch
-  cmMenuGerman = 1006;   // Deutsch
+cmAbout = 1001; // Display About
+cmPara = 1003; // Parameters
+cmMenuEnlish = 1005; // English
+cmMenuGerman = 1006; // German
 
 type
 (*
-Deklaration der Komponenten
+Component declaration
 *)
-  //code+
-  TMyApp = object(TApplication)
-    procedure InitStatusLine; virtual;                 // Statuszeile
-    procedure InitMenuBar; virtual;                    // Menü
-    procedure HandleEvent(var Event: TEvent); virtual; // Eventhandler
-  private
-    menuGer, menuEng: PMenuView;          // Die beiden Menüs
-    StatusGer, StatusEng: PStatusLine;    // Die beiden Stauszeilen
-  end;
-  //code-
+
+/code+
+
+TMyApp = object(TApplication)
+
+procedure InitStatusLine; virtual; // Status bar
+procedure InitMenuBar; virtual; // Menu
+
+procedure HandleEvent(var Event: TEvent); virtual; // Event handler
+
+private
+
+menuGer, menuEng: PMenuView; // The two menus
+
+StatusGer, StatusEng: PStatusLine; // The two status lines
+
+end;
+
+/code-
 
 (*
-Inizialisieren der beiden Statuszeilen.
+Initialize the two status lines.
+
 *)
-  //code+
-  procedure TMyApp.InitStatusLine;
-  var
-    R: TRect;
-  begin
-    GetExtent(R);
-    R.A.Y := R.B.Y - 1;
 
-    // Statuszeile deutsch
-    StatusGer := New(PStatusLine, Init(R, NewStatusDef(0, $FFFF,
-      NewStatusKey('~Alt+X~ Programm beenden', kbAltX, cmQuit,
-      NewStatusKey('~F10~ Menue', kbF10, cmMenu,
-      NewStatusKey('~F1~ Hilfe', kbF1, cmHelp, nil))), nil)));
+/code+
 
-    // Statuszeile englisch
-    StatusEng := New(PStatusLine, Init(R, NewStatusDef(0, $FFFF,
-      NewStatusKey('~Alt+X~ Exit', kbAltX, cmQuit,
-      NewStatusKey('~F10~ Menu', kbF10, cmMenu,
-      NewStatusKey('~F1~ Help', kbF1, cmHelp, nil))), nil)));
+procedure TMyApp.InitStatusLine;
 
-    StatusLine := StatusGer; // Deutsch per Default
-  end;
-  //code-
-
-(*
-Inizialisieren der beiden Menüs.
-*)
-  //code+
-  procedure TMyApp.InitMenuBar;
-  var
-    R: TRect;
-  begin
-    GetExtent(R);
-    R.B.Y := R.A.Y + 1;
-
-    // Menü deutsch
-    menuGer := New(PMenuBar, Init(R, NewMenu(
-      NewSubMenu('~D~atei', hcNoContext, NewMenu(
-        NewItem('S~c~hliessen', 'Alt-F3', kbAltF3, cmClose, hcNoContext,
-        NewLine(
-        NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil)))),
-      NewSubMenu('~O~ptionen', hcNoContext, NewMenu(
-        NewItem('~P~arameter...', '', kbF2, cmPara, hcNoContext,
-        NewLine(
-        NewItem('~D~eutsch', 'Alt-D', kbAltD, cmMenuGerman, hcNoContext,
-        NewItem('~E~nglisch', 'Alt-E', kbAltE, cmMenuEnlish, hcNoContext, nil))))),
-      NewSubMenu('~H~ilfe', hcNoContext, NewMenu(
-        NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil)), nil))))));
-
-    // Menü englisch
-    menuEng := New(PMenuBar, Init(R, NewMenu(
-      NewSubMenu('~F~ile', hcNoContext, NewMenu(
-        NewItem('~C~lose', 'Alt-F3', kbAltF3, cmClose, hcNoContext,
-        NewLine(
-        NewItem('E~x~it', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil)))),
-      NewSubMenu('~O~ptions', hcNoContext, NewMenu(
-        NewItem('~P~arameters...', '', kbF2, cmPara, hcNoContext,
-        NewLine(
-        NewItem('German', 'Alt-D', kbAltD, cmMenuGerman, hcNoContext,
-        NewItem('English', 'Alt-E', kbAltE, cmMenuEnlish, hcNoContext, nil))))),
-      NewSubMenu('~H~elp', hcNoContext, NewMenu(
-        NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil)), nil))))));
-
-    MenuBar := menuGer; // Deutsch per Default
-  end;
-//code-
-
-(*
-Austauschen der Komponenten
-*)
-//code+
-  procedure TMyApp.HandleEvent(var Event: TEvent);
-  var
-    Rect: TRect;              // Rechteck für die Statuszeilen Position.
-
-  begin
-    GetExtent(Rect);
-
-    Rect.A.Y := Rect.B.Y - 1;
-    inherited HandleEvent(Event);
-
-    if Event.What = evCommand then begin
-      case Event.Command of
-        cmAbout: begin
-          // Ein About Dialog
-        end;
-
-        // Menü auf englisch
-        cmMenuEnlish: begin
-
-          // Menü tauschen
-          Delete(MenuBar);          // Altes Menü entfernen
-          MenuBar := menuEng;       // Neues Menü zuordnen
-          Insert(MenuBar);          // Neues Menü einfügen
-
-          // Statuszeile tauschen
-          Delete(StatusLine);       // Alte Statuszeile entfernen
-          StatusLine := StatusEng;  // Neue Statuszeile zuordnen
-          Insert(StatusLine);       // Neue Statuszeile einfügen
-        end;
-
-        // Menü auf deutsch
-        cmMenuGerman: begin
-          Delete(MenuBar);
-          MenuBar := menuGer;
-          Insert(MenuBar);
-
-          Delete(StatusLine);
-          StatusLine := StatusGer;
-          Insert(StatusLine);
-        end;
-        cmPara: begin
-          // Ein Parameter Dialog
-        end;
-        else begin
-          Exit;
-        end;
-      end;
-    end;
-    ClearEvent(Event);
-  end;
-//code-
 var
-  MyApp: TMyApp;
+
+R: TRect;
 
 begin
-  MyApp.Init;   // Inizialisieren
-  MyApp.Run;    // Abarbeiten
-  MyApp.Done;   // Freigeben
+
+GetExtent(R);
+
+R.A.Y := R.B.Y - 1;
+
+
+// Status line in German 
+StatusGer := New(PStatusLine, Init(R, NewStatusDef(0, $FFFF, 
+NewStatusKey('~Alt+X~ Quit program', kbAltX, cmQuit, 
+NewStatusKey('~F10~ Menu', kbF10, cmMenu, 
+NewStatusKey('~F1~ Help', kbF1, cmHelp, nil))), nil))); 
+
+// Status line in English 
+StatusEng := New(PStatusLine, Init(R, NewStatusDef(0, $FFFF, 
+NewStatusKey('~Alt+X~ Exit', kbAltX, cmQuit, 
+NewStatusKey('~F10~ Menu', kbF10, cmMenu, 
+NewStatusKey('~F1~ Help', kbF1, cmHelp, nil))), nil))); 
+
+StatusLine := StatusGer; // German by default
+
+end;
+
+//code-
+
+(*
+Initialize the two menus.
+
+*)
+
+//code+
+
+procedure TMyApp.InitMenuBar;
+
+var
+R: TRect;
+
+begin
+
+GetExtent(R);
+
+R.B.Y := R.A.Y + 1;
+
+
+// Menu German 
+menuGer := New(PMenuBar, Init(R, NewMenu( 
+NewSubMenu('~File', hcNoContext, NewMenu( 
+NewItem('S~c~hliessen', 'Alt-F3', kbAltF3, cmClose, hcNoContext, 
+NewLine( 
+NewItem('~B~end', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil)))), 
+NewSubMenu('~Options', hcNoContext, NewMenu( 
+NewItem('~Parameter...', '', kbF2, cmPara, hcNoContext, 
+NewLine( 
+NewItem('~D~eutsch', 'Alt-D', kbAltD, cmMenuGerman, hcNoContext, 
+NewItem('~English', 'Alt-E', kbAltE, cmMenuEnlish, hcNoContext, nil))))), 
+NewSubMenu('~Help', hcNoContext, NewMenu( 
+NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil)), nil)))))); 
+
+// Menu in English 
+menuEng := New(PMenuBar, Init(R, NewMenu( 
+NewSubMenu('~F~ile', hcNoContext, NewMenu( 
+NewItem('~C~lose', 'Alt-F3', kbAltF3, cmClose, hcNoContext, 
+NewLine( 
+NewItem('E~x~it', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil)))), 
+NewSubMenu('~O~ptions', hcNoContext, NewMenu( 
+NewItem('~P~arameters...', '', kbF2, cmPara, hcNoContext, 
+NewLine( 
+NewItem('German', 'Alt-D', kbAltD, cmMenuGerman, hcNoContext, 
+NewItem('English', 'Alt-E', kbAltE, cmMenuEnlish, hcNoContext, nil))))),
+
+NewSubMenu('~H~elp', hcNoContext, NewMenu(
+NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil)), nil))))));
+
+MenuBar := menuGer; // German by default
+
+end;
+
+//code-
+
+(*
+Replacing the components
+*)
+
+//code+
+
+procedure TMyApp.HandleEvent(var Event: TEvent);
+
+var
+Rect: TRect; // Rectangle for the status bar position.
+
+begin
+
+GetExtent(Rect);
+
+Rect.A.Y := Rect.B.Y - 1;
+
+inherited HandleEvent(Event);
+
+if Event.What = evCommand then begin
+
+case Event.Command of
+
+cmAbout: begin
+
+/ An About dialog
+
+end;
+
+/ // Menu in English
+cmMenuEnlish: begin
+
+/ // Swap menu
+
+Delete(MenuBar); // Remove old menu
+
+MenuBar := menuEng; // Assign new menu
+
+Insert(MenuBar); // Insert new menu
+
+/ // Swap status line
+
+Delete(StatusLine); // Remove old status line
+
+StatusLine := StatusEng; // Assign new status line
+
+Insert(StatusLine); // Insert new status line
+end;
+
+/ // Menu in German
+cmMenuGerman: begin
+
+Delete(MenuBar);
+
+MenuBar := menuGer;
+
+Insert(MenuBar);
+
+Delete(StatusLine);
+
+StatusLine := StatusGer;
+
+Insert(StatusLine);
+
+end;
+
+cmPara: begin
+
+/ // A parameter dialog
+end;
+
+else begin
+
+Exit;
+
+end;
+
+end;
+
+end;
+
+ClearEvent(Event);
+
+end;
+
+//code-
+
+var
+MyApp: TMyApp;
+
+begin
+
+MyApp.Init; // Initialize
+
+MyApp.Run; // Execute
+
+MyApp.Done; // Release
+
 end.
