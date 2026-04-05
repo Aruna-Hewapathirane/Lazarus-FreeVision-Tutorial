fd1cd58bdf22c98e8ba0ba965ec1688fcd0df3df
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-04-05 16:09:41 -0400

    Update mydialog.pas

--- 04_-_Dialoge_als_Komponente/00_-_Ein_einfaches_About/mydialog.pas
+++ 04_-_Dialoge_als_Komponente/00_-_Ein_einfaches_About/mydialog.pas
@@ -1,43 +1,54 @@
 //head+
 unit MyDialog;
-//head-
+
+/head-
 
 //interface+
 interface
 
 uses
-  App, Objects, Drivers, Views, Dialogs;
+App, Objects, Drivers, Views, Dialogs;
 
 type
-  PMyAbout = ^TMyAbout;
-  TMyAbout = object(TDialog)
-    constructor Init;  // Neuer Konstruktor, welche den Dialog mit den Komponenten baut.
-  end;
+PMyAbout = ^TMyAbout;
+
+TMyAbout = object(TDialog)
+
+constructor Init; // New constructor that builds the dialog with the components.
+
+end;
+
 //interface-
 
 //implementation+
 implementation
 
 constructor TMyAbout.Init;
+
 var
-  R: TRect;
+R: TRect;
+
 begin
-  R.Assign(0, 0, 42, 11);
-  R.Move(23, 3);
 
-  inherited Init(R, 'About');  // Dialog in verdefinierter Grösse erzeugen.
+R.Assign(0, 0, 42, 11);
 
-  // StaticText
-  R.Assign(5, 2, 41, 8);
-  Insert(new(PStaticText, Init(R,
-    'Free Vison Tutorial 1.0' + #13 +
-    '2017' + #13 +
-    'Gechrieben von M. Burkhard' + #13#32#13 +
-    'FPC: '+ {$I %FPCVERSION%} + '   OS:'+ {$I %FPCTARGETOS%} + '   CPU:' + {$I %FPCTARGETCPU%})));
+R.Move(23, 3);
 
-  // Ok-Button
-  R.Assign(27, 8, 37, 10);
-  Insert(new(PButton, Init(R, '~O~K', cmOK, bfDefault)));
+inherited Init(R, 'About'); // Create a dialog of a defined size.
+
+// StaticText
+
+R.Assign(5, 2, 41, 8);
+
+Insert(new(PStaticText, Init(R, 
+'Free Vision Tutorial 1.0' + #13 + 
+'2017' + #13 + 
+'Written by M. Burkhard' + #13#32#13 + 
+'FPC: '+ {$I %FPCVERSION%} + ' OS:'+ {$I %FPCTARGETOS%} + ' CPU:' + {$I %FPCTARGETCPU%}))); 
+
+// Ok button 
+R.Assign(27, 8, 37, 10); 
+Insert(new(PButton, Init(R, '~O~K', cmOK, bfDefault)));
 end;
 //implementation-
 
