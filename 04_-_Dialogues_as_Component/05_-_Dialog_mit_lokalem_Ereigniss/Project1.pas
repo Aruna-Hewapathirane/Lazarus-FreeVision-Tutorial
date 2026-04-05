ed94739681772b6552cfe7c5923f115062d2b57e
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-04-05 16:34:39 -0400

    Update Project1.pas

--- 04_-_Dialogues_as_Component/05_-_Dialog_mit_lokalem_Ereigniss/Project1.pas
+++ 04_-_Dialogues_as_Component/05_-_Dialog_mit_lokalem_Ereigniss/Project1.pas
@@ -1,125 +1,187 @@
 //image image.png
 (*
-In den vererbten Dialogen ist es möglich Buttons einzubauen, welche lokal im Dialog eine Aktion ausführen.
-Im Beispiel wir eine MessageBox aufgerufen.
+In inherited dialogs, it's possible to include buttons that perform an action locally within the dialog.
+In this example, a MessageBox is displayed.
+
 *)
-//lineal
+//ruleral
 program Project1;
 
 uses
-  App,      // TApplication
-  Objects,  // Fensterbereich (TRect)
-  Drivers,  // Hotkey
-  Views,    // Ereigniss (cmQuit)
-  Menus,    // Statuszeile
-  MsgBox,   // Messageboxen
-  Dialogs,  // Dialoge
-  MyDialog;
+App, // TApplication
+Objects, // Window area (TRect)
+Drivers, // Hotkey
+Views, // Events (cmQuit)
+Menus, // Status bar
+MsgBox, // Message boxes
+Dialogs, // Dialogs
+MyDialog;
 
 const
-  cmAbout = 1001;     // About anzeigen
+cmAbout = 1001; // Display About
 
 type
-  TMyApp = object(TApplication)
-    procedure InitStatusLine; virtual;                 // Statuszeile
-    procedure InitMenuBar; virtual;                    // Menü
-    procedure HandleEvent(var Event: TEvent); virtual; // Eventhandler
-    procedure OutOfMemory; virtual;                    // Wird aufgerufen, wen Speicher überläuft.
-  end;
+TMyApp = object(TApplication)
 
-  procedure TMyApp.InitStatusLine;
-  var
-    R: TRect;              // Rechteck für die Statuszeilen Position.
-  begin
-    GetExtent(R);
-    R.A.Y := R.B.Y - 1;
+procedure InitStatusLine; virtual; // Status bar
 
-    StatusLine := New(PStatusLine, Init(R, NewStatusDef(0, $FFFF,
-      NewStatusKey('~Alt+X~ Programm beenden', kbAltX, cmQuit,
-      NewStatusKey('~F10~ Menu', kbF10, cmMenu,
-      NewStatusKey('~F1~ About...', kbF1, cmAbout, nil))), nil)));
-  end;
+procedure InitMenuBar; virtual; // Menu
 
-  procedure TMyApp.InitMenuBar;
-  var
-    R: TRect;                       // Rechteck für die Menüzeilen-Position.
-  begin
-    GetExtent(R);
-    R.B.Y := R.A.Y + 1;
+procedure HandleEvent(var Event: TEvent); virtual; // Event handler
 
-    MenuBar := New(PMenuBar, Init(R, NewMenu(
-      NewSubMenu('~D~atei', hcNoContext, NewMenu(
-        NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil)),
-      NewSubMenu('~H~ilfe', hcNoContext, NewMenu(
-        NewItem('~A~bout...', '', kbNoKey, cmAbout, hcNoContext, nil)), nil)))));
-  end;
+procedure OutOfMemory; virtual; // Called when memory overflows.
 
-(*
-Im Hauptprogramm ändert sich nichts daran, dem ist egal, ob lokal noch etwas gemacht wird.
-*)
-//code+
-  procedure TMyApp.HandleEvent(var Event: TEvent);
-  var
-    AboutDialog: PMyAbout;
-  begin
-    inherited HandleEvent(Event);
+end;
 
-    if Event.What = evCommand then begin
-      case Event.Command of                   // About Dialog
-        cmAbout: begin
-          AboutDialog := New(PMyAbout, Init);
-          if ValidView(AboutDialog) <> nil then begin // Prüfen ob genügend Speicher.
-            Desktop^.ExecView(AboutDialog);           // Dialog About ausführen.
-            Dispose(AboutDialog, Done);               // Dialog und Speicher frei geben.
-          end;
-        end;
-        else begin
-          Exit;
-        end;
-      end;
-    end;
-    ClearEvent(Event);
-  end;
-//code-
-
-  procedure TMyApp.OutOfMemory;
-  begin
-    MessageBox('Zu wenig Arbeitsspeicher !', nil, mfError + mfOkButton);
-  end;
+procedure TMyApp.InitStatusLine;
 
 var
-  MyApp: TMyApp;
+
+R: TRect; // Rectangle for the status bar position.
 
 begin
-  MyApp.Init;   // Inizialisieren
-  MyApp.Run;    // Abarbeiten
-  MyApp.Done;   // Freigeben
 
-//lineal
+GetExtent(R);
+
+R.A.Y := R.B.Y - 1;
+
+StatusLine := New(PStatusLine, Init(R, NewStatusDef(0, $FFFF,
+
+NewStatusKey('~Alt+X~ Exit Program', kbAltX, cmQuit,
+
+NewStatusKey('~F10~ Menu', kbF10, cmMenu,
+
+NewStatusKey('~F1~ About...', kbF1, cmAbout, nil))), nil)));
+
+end;
+
+procedure TMyApp.InitMenuBar;
+
+var
+
+R: TRect; // Rectangle for the menu bar position.
+
+begin
+
+GetExtent(R);
+
+R.B.Y := R.A.Y + 1;
+
+MenuBar := New(PMenuBar, Init(R, NewMenu(
+NewSubMenu('~D~atei', hcNoContext, NewMenu(
+NewItem('~Exit', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil)),
+
+NewSubMenu('~Help', hcNoContext, NewMenu(
+NewItem('~About...', '', kbNoKey, cmAbout, hcNoContext, nil)), nil)))));
+end;
+
+
+(*) Nothing changes in the main program; it doesn't care if something else is done locally.
+
+*)
+
+//code+
+
+procedure TMyApp.HandleEvent(var Event: TEvent);
+
+var
+
+AboutDialog: PMyAbout;
+
+begin
+
+inherited HandleEvent(Event);
+
+
+``` if Event.What = evCommand then begin
+
+case Event.Command of // About Dialog
+
+cmAbout: begin
+
+AboutDialog := New(PMyAbout, Init);
+
+if ValidView(AboutDialog) <> nil then begin // Check if there is enough memory.
+
+Desktop^.ExecView(AboutDialog); // Execute the About dialog.
+
+Dispose(AboutDialog, Done); // Release the dialog and memory.
+
+end;
+
+end;
+
+else begin
+
+Exit;
+
+end;
+
+end;
+
+end;
+
+ClearEvent(Event);
+
+end;
+
+//code-
+
+procedure TMyApp.OutOfMemory;
+
+begin
+
+MessageBox('Not enough memory!', nil, mfError + mfOkButton);
+
+end;
+
+var
+
+MyApp: TMyApp;
+
+begin
+
+MyApp.Init; // Initialize
+
+MyApp.Run; // Processing
+
+MyApp.Done; // Release
+
+//ruler
 (*
-<b>Unit mit dem neuen Dialog.</b>
+<b>Unit with the new dialog.</b>
+
 <br>
-Dort sieht man gut, das es ein Button für lokale Ereignisse hat.
-Wichtig ist, bei den Nummernvergabe, das sich dies nicht mit einem anderen Eventnummer überschneidet.
-Vor allem dann, wen der Dialog nicht Modal geöffnet wird.
-Ausser es ist gewünscht, wen man zB. über das Menü auf den Dialog zugreifen will.
+You can clearly see that it has a button for local events.
+
+It's important that the numbering doesn't overlap with another event number.
+
+Especially if the dialog isn't opened modally.
+
+Unless it's desired, for example, if you want to access the dialog via the menu.
+
 *)
 //includepascal mydialog.pas head
 
 (*
-Für den Dialog kommt noch ein HandleEvent hinzu.
+A HandleEvent is added for the dialog.
+
 *)
 //includepascal mydialog.pas type
 
 (*
-Im Konstruktor wird der Dialog noch um den Button Msg-box ergänzt, welcher das lokale Ereigniss <b>cmMsg</b> abarbeitet.
+In the constructor, the dialog is further enhanced with the Msg-box button, which handles the local event <b>cmMsg</b>.
+
 *)
 //includepascal mydialog.pas init
 
 (*
-Im neuen EventHandle, werden loake Ereigniss (cmMsg) abarbeitet.
-Andere Ereignisse, zB. <b>cmOk</b> wird an das Hauptprogramm weiter gereicht, welches dann den Dialog auch schliesst.
+In the new EventHandle, local events (cmMsg) are handled. The dialog is processed.
+
+Other events, e.g., <b>cmOk</b>, are passed to the main program, which then closes the dialog.
+
 *)
+
 //includepascal mydialog.pas handleevent
 
 end.
