1efbe07b2b95b8f3ea1accc16d7614b0182a39a8
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-04-14 08:46:15 -0400

    Update Project1.pas

--- 04_-_Dialogues_as_Component/15_-_Verschiedene_Dialog_Farben/Project1.pas
+++ 04_-_Dialogues_as_Component/15_-_Verschiedene_Dialog_Farben/Project1.pas
@@ -1,123 +1,180 @@
 //image image.png
 (*
-Einem Fenster/Dialog, kann man verschiedene FarbeSchema zuordnen.
-Standardmässig wird folgendes verwendet:
+Different color schemes can be assigned to windows/dialogs.
+By default, the following is used:
+
 //code+
-Editor-Fenster : Blau
-Dialog         : Grau
-Hilfe-Fenster  : Cyan
+Editor window: Blue
+Dialog: Gray
+Help window: Cyan
+
 //code-
 
-Ohne Zutun, kommen die Fenster/Dialog immer in der richtigen Farbe.
-Eine Modifizierung ist nur in speziellen Fällen von Sinnen.
+Without any action, windows/dialogs always appear in the correct color. Modifying this is only useful in special cases.
+
 *)
 program Project1;
 
 uses
-  App,      // TApplication
-  Objects,  // Fensterbereich (TRect)
-  Drivers,  // Hotkey
-  Views,    // Ereigniss (cmQuit)
-  Menus,    // Statuszeile
-  MsgBox,   // Messageboxen
-  Dialogs,  // Dialoge
-  MyDialog;
+
+App, // TApplication
+
+Objects, // Window area (TRect)
+
+Drivers, // Hotkey
+
+Views, // Events (cmQuit)
+
+Menu, // Status bar
+
+MsgBox, // Message boxes
+
+Dialogs, // Dialogs
+
+MyDialog;
 
 const
-  cmAbout = 1001;     // About anzeigen
+
+cmAbout = 1001; // Display About
 
 type
-  TMyApp = object(TApplication)
-    procedure InitStatusLine; virtual;                 // Statuszeile
-    procedure InitMenuBar; virtual;                    // Menü
-    procedure HandleEvent(var Event: TEvent); virtual; // Eventhandler
-    procedure OutOfMemory; virtual;                    // Wird aufgerufen, wen Speicher überläuft.
-  end;
 
-  procedure TMyApp.InitStatusLine;
-  var
-    R: TRect;              // Rechteck für die Statuszeilen Position.
-  begin
-    GetExtent(R);
-    R.A.Y := R.B.Y - 1;
+TMyApp = object(TApplication)
 
-    StatusLine := New(PStatusLine, Init(R, NewStatusDef(0, $FFFF,
-      NewStatusKey('~Alt+X~ Programm beenden', kbAltX, cmQuit,
-      NewStatusKey('~F10~ Menu', kbF10, cmMenu,
-      NewStatusKey('~F1~ About...', kbF1, cmAbout, nil))), nil)));
-  end;
+procedure InitStatusLine; virtual; // Status bar
 
-  procedure TMyApp.InitMenuBar;
-  var
-    R: TRect;                       // Rechteck für die Menüzeilen-Position.
-  begin
-    GetExtent(R);
-    R.B.Y := R.A.Y + 1;
+procedure InitMenuBar; virtual; // Menu
 
-    MenuBar := New(PMenuBar, Init(R, NewMenu(
-      NewSubMenu('~D~atei', hcNoContext, NewMenu(
-        NewItem('~B~eenden', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil)),
-      NewSubMenu('~O~ption', hcNoContext, NewMenu(
-        NewItem('Dia~l~og...', '', kbNoKey, cmAbout, hcNoContext, nil)), nil)))));
-  end;
+procedure HandleEvent(var Event: TEvent); virtual; // Event handler
 
-  procedure TMyApp.HandleEvent(var Event: TEvent);
-  var
-    MyDialog: PMyDialog;
-  begin
-    inherited HandleEvent(Event);
+procedure OutOfMemory; virtual; // Called when memory overflows.
 
-    if Event.What = evCommand then begin
-      case Event.Command of                   // About Dialog
-        cmAbout: begin
-          MyDialog := New(PMyDialog, Init);
-          if ValidView(MyDialog) <> nil then begin // Prüfen ob genügend Speicher.
-            Desktop^.ExecView(MyDialog);           // Dialog ausführen.
-            Dispose(MyDialog, Done);               // Dialog und Speicher frei geben.
-          end;
-        end;
-        else begin
-          Exit;
-        end;
-      end;
-    end;
-    ClearEvent(Event);
-  end;
+end;
 
-  procedure TMyApp.OutOfMemory;
-  begin
-    MessageBox('Zu wenig Arbeitsspeicher !', nil, mfError + mfOkButton);
-  end;
+procedure TMyApp.InitStatusLine;
 
 var
-  MyApp: TMyApp;
+
+R: TRect; // Rectangle for the status line position.
 
 begin
-  MyApp.Init;   // Inizialisieren
-  MyApp.Run;    // Abarbeiten
-  MyApp.Done;   // Freigeben
 
-//lineal
+GetExtent(R);
+
+R.A.Y := R.B.Y - 1;
+
+StatusLine := New(PStatusLine, Init(R, NewStatusDef(0, $FFFF,
+
+NewStatusKey('~Alt+X~ Exit Program', kbAltX, cmQuit,
+
+NewStatusKey('~F10~ Menu', kbF10, cmMenu,
+
+NewStatusKey('~F1~ About...', kbF1, cmAbout, nil))), nil)));
+
+end;
+
+procedure TMyApp.InitMenuBar;
+
+var
+
+R: TRect; // Rectangle for the menu bar position.
+
+begin 
+GetExtent(R); 
+R.B.Y := R.A.Y + 1; 
+
+MenuBar := New(PMenuBar, Init(R, NewMenu( 
+NewSubMenu('~File', hcNoContext, NewMenu( 
+NewItem('~B~end', 'Alt-X', kbAltX, cmQuit, hcNoContext, nil)), 
+NewSubMenu('~O~ption', hcNoContext, NewMenu( 
+NewItem('Dia~l~og...', '', kbNoKey, cmAbout, hcNoContext, nil)), nil))))); 
+end; 
+
+procedure TMyApp.HandleEvent(var Event: TEvent); 
+var 
+MyDialog: PMyDialog; 
+begin 
+inherited HandleEvent(Event); 
+
+if Event.What = evCommand then begin
+
+case Event.Command of // About Dialog
+
+cmAbout: begin
+
+MyDialog := New(PMyDialog, Init);
+
+if ValidView(MyDialog) <> nil then begin // Check if there is enough memory.
+
+Desktop^.ExecView(MyDialog); // Execute the dialog.
+
+Dispose(MyDialog, Done); // Release the dialog and memory.
+
+end;
+
+end;
+
+else begin
+
+Exit;
+
+end;
+
+end;
+
+end;
+
+ClearEvent(Event);
+
+end;
+
+procedure TMyApp.OutOfMemory;
+
+begin
+
+MessageBox('Not enough memory!', nil, mfError + mfOkButton);
+
+end;
+
+var
+
+MyApp: TMyApp;
+
+begin
+
+MyApp.Init; // Initialize
+
+MyApp.Run; // Execute
+
+MyApp.Done; // Release
+
+//ruler
 (*
-<b>Unit mit dem neuen Dialog.</b>
+<b>Unit with the new dialog.</b>
+
 <br>
-Mit den 3 oberen Button, kann man das Farb-Schema des Dialoges ändern.
+The three buttons at the top allow you to change the dialog's color scheme.
+
 *)
 //includepascal mydialog.pas head
 
 (*
-Hier sind 3 Event-Konstante hinzugekommen.
+Three event constants have been added here.
+
 *)
 //includepascal mydialog.pas type
 
 (*
-Das Bauen des Dialoges ist nichts besonderes.
+Building the dialog is nothing special.
+
 *)
 //includepascal mydialog.pas init
 
 (*
-Hier werden die Farb-Schemas mit Hilfe von <b>Palette := dpxxx</b> geändert.
-Auch hier ist wichtig, das man <b>Draw</b> aufruft, diemal nicht für eine Komponente, sonder für den ganzen Dialog.
+Here, the color schemes are changed using <b>Palette := dpxxx</b>.
+
+It's also important to call <b>Draw</b> here, this time not for a single component, but for the entire dialog.
+
 *)
 //includepascal mydialog.pas handleevent
 
